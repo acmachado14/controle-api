979e382a1720a0897f886c1819d2ffdccf2ba3eb
Angelo <cupertinoangelo13@gmail.com>
2022-05-15 17:32:10 -0300

    :recycle: refactor: Alterar controller base

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Support\Constants\StatusHTTP;
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    protected $classe;
+
+    public function index()
+    {
+        return $this->classe::all();
+    }
+
+    public function store(Request $request)
+    {
+        return response()
+            ->json(
+                $this->classe::create($request->all()),
+                StatusHTTP::CREATED
+            );
+    }
+
+    public function show(int $id)
+    {
+        $recurso = $this->classe::find($id);
+        if (is_null($recurso)) {
+            return response()->json('', StatusHTTP::NO_CONTENT);
+        }
+
+        return response()->json($recurso);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $recurso = $this->classe::find($id);
+        if (is_null($recurso)) {
+            return response()->json([
+                'erro' => 'Recurso não encontrado'
+            ], StatusHTTP::NOT_FOUND);
+        }
+        $recurso->fill($request->all());
+        $recurso->save();
+
+        return $recurso;
+    }
+
+    public function destroy(int $id)
+    {
+        $qtdRecursosRemovidos = $this->classe::destroy($id);
+        if ($qtdRecursosRemovidos === 0) {
+            return response()->json([
+                'erro' => 'Recurso não encontrado'
+            ], StatusHTTP::NOT_FOUND);
+        }
+
+        return response()->json('', StatusHTTP::NO_CONTENT);
+    }
 }
