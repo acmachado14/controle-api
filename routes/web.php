c7fa2ce69478f76ae315d30933b389215ecd6c4b
Angelo <cupertinoangelo13@gmail.com>
2022-05-15 17:34:47 -0300

    :guitar: feat: Criar rotas iniciais

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+
+    $router->group(['prefix' => 'usuario'], function () use ($router) {
+        $router->get('', 'UsuarioController@index');
+        $router->post('', 'UsuarioController@store');
+        $router->get('{id}', 'UsuarioController@show');
+        $router->put('{id}', 'UsuarioController@update');
+        $router->delete('{id}', 'UsuarioController@destroy');
+        $router->get('{idUsuario}/contas', 'ContaController@contasPorUsuario');
+        $router->get('{idUsuario}/contas', 'ContaController@contasEmConjunto');
+    });
+
+    $router->group(['prefix' => 'conta'], function () use ($router) {
+        $router->get('', 'ContaController@index');
+        $router->post('', 'ContaController@store');
+        $router->get('{id}', 'ContaController@show');
+        $router->put('{id}', 'ContaController@update');
+        $router->delete('{id}', 'ContaController@destroy');
+    });
+
+});
